8a5dc1b7eaf4689c164fb1f8b86e36bbb7a5dfd4
sp14031983 <sp14031983@gmail.com>
2025-10-31 18:28:45 +0530

    Validate workflow

--- src/main/java/com/db/TradeStoreApplication.java
+++ src/main/java/com/db/TradeStoreApplication.java
@@ -3,6 +3,7 @@ package com.db;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Entry class
 @SpringBootApplication
 public class TradeStoreApplication {
     public static void main(String[] args) {
